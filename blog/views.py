946a20b318be6a96f737da5b2111fcdd2a5557eb
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2022-09-29 23:55:51 +0200

    Switch to extendable template structure for pages

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from blog.models import BlogPost
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html', {'data': BlogPost.objects.all()})
+    return render(request, 'blog/post_index.html', {'data': BlogPost.objects.all()})
 
 def post(request, blogpost_id):
     selected_post = get_object_or_404(BlogPost, id=blogpost_id)
