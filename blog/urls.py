83a985be1eac7ccc4a177b6f67aaeb7a2abd34ff
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2022-10-04 23:30:49 +0200

    Added rudementary user login functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('login/',
+    auth_views.LoginView.as_view(
+      template_name='blog/login.html',
+      next_page="blog:index"
+      ),
+    name='login'
+  ),
   path('<slug:blogpost_slug>/', views.post, name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
+import imp
 from select import select
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import login, authenticate
 from blog.models import BlogPost
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/post_index.html', {'data': BlogPost.objects.order_by('-pub_date')})
+    return render(request, 'blog/post_index.html', {'data': BlogPost.objects.order_by('-pub_date'), 'user': request.user})
 
 def post(request, blogpost_slug):
     selected_post = get_object_or_404(BlogPost, slug=blogpost_slug)
