8c87466a8f53baafec850799d892156b3f34131c
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2022-10-05 19:27:51 +0200

    Remove unused accidental imports

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,3 @@
-import imp
-from select import select
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import login, authenticate
