f90af6d0cf9ab1fa07b0ecde4f014d21029dcd01
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2022-10-05 20:42:29 +0200

    Conditionally allow for post edit only if user is logged in

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,18 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from blog.models import BlogPost
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/post_index.html', {'data': BlogPost.objects.order_by('-pub_date'), 'user': request.user})
+    return render(request, 'blog/post_index.html', {'data': BlogPost.objects.order_by('-pub_date')})
 
 def post(request, blogpost_slug):
     selected_post = get_object_or_404(BlogPost, slug=blogpost_slug)
     return render(request, 'blog/view_post.html', {'post': selected_post})
 
+@login_required(login_url="blog:login")
 def edit_post(request, blogpost_slug):
     selected_post = get_object_or_404(BlogPost, slug=blogpost_slug)
 
