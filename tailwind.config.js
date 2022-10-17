244d52e02d175a6383b8ba6f8c980cace04505a5
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2022-10-17 20:11:25 +0200

    Navbar rewritten with TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./blog/{static,templates}/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['Roboto', 'arial'],
+      'serif': ['"Roboto Serif"', '"Times New Roman"'],
+      'mono': ['"Roboto Mono"', "monospace"],
+      'display': ['Inter', 'Roboto', 'Arial']
+    },
     extend: {},
   },
   plugins: [
